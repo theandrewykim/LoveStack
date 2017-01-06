ebd8de83478c1eaee428bb505c8b047cdb0c8d94
theandrewykim <theandrewkimm@gmail.com>
2017-01-05 17:01:54 -0800

    add users

--- db/migrate/20160325153807_create_users.rb
+++ db/migrate/20160325153807_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email, uniqueness: true, null: false
       t.string :password_digest, null: false
-
+      
       t.timestamps null: false
     end
   end
